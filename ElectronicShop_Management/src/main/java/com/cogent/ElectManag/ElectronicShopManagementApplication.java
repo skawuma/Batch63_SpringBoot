05b5a97646b30d7b0cbe643c0a468a6f06650d72
skawuma <82976186+skawuma@users.noreply.github.com>
2022-10-22 16:26:42 -0400

    Still working on

---
+++ ElectronicShop_Management/src/main/java/com/cogent/ElectManag/ElectronicShopManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cogent.ElectManag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicShopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicShopManagementApplication.class, args);
+	}
+
+}
---
+++ ElectronicShop_Management/src/test/java/com/cogent/ElectManag/ElectronicShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogent.ElectManag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
