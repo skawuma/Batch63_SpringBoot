eea363471c054540eba0f0a53e957ba31036a9f9
samuelkawuma <samuelkawuma@localhost>
2022-11-07 15:56:25 -0500

    working on

--- EmpManage.zip_expanded/EmpManage/src/main/java/com/example/EmpManage/controller/EmployeeController.java
+++ EmpManage.zip_expanded/EmpManage/src/main/java/com/example/EmpManage/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 //import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.example.EmpManage.entity.Employee;
 import com.example.EmpManage.repo.EmployRepository;
 import com.example.EmpManage.service.EmployeeService;
 @RestController
+@CrossOrigin
 @RequestMapping("/api/employee")
 public class EmployeeController {
 
--- EmpManage.zip_expanded/EmpManage/src/main/java/com/example/EmpManage/controller/PayrollController.java
+++ EmpManage.zip_expanded/EmpManage/src/main/java/com/example/EmpManage/controller/PayrollController.java
@@ -3,6 +3,7 @@ package com.example.EmpManage.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import com.example.EmpManage.entity.Payroll;
 import com.example.EmpManage.repo.PayrollRepository;
 
 @RestController
-
+@CrossOrigin
 @RequestMapping("/api/payroll")
 public class PayrollController {
 	
